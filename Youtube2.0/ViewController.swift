c244f56ef0544fec62cc879c4923e47af085ef01
Ronald Hernandez <2ronald.hernandez@gmail.com>
2017-01-27 08:01:08 -0600

    Initial Commit

---
+++ Youtube2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Youtube2.0
+//
+//  Created by Ronald Hernandez on 1/27/17.
+//  Copyright © 2017 Ronaldoh1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
